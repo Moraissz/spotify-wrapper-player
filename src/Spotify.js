9e2eb48e79deb2256d500e15149e5d50aebc3b87
Lucas Morais <lucas.moraissz07@gmail.com>
2018-05-08 21:31:30 -0300

    Add spotify-wrapper library

---
+++ src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-wrapper-jswithtddcourse';
+
+const spotify = new SpotifyWrapper({
+  token: 'BQAIGEmOZ647qviqg2B8q_92UXxSlqNP_RsfHMsuz4Vu3VGwasV4WDYqIHAek5e29B6G27UfwkVXGv9madrrXYFl9SHvZokYd5a8VOres3jpO-qf14uEmcO0JLp_fkYXVbbj_SoC',
+});
+
+export default spotify;
